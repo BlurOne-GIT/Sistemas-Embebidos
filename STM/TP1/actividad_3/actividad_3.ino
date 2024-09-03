3f434bbd262eca5c02640be54e4410e92786ee4d
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-09-03 16:27:46 -0300

    feat(STM/TP1): actividad 3

---
+++ STM/TP1/actividad_3/actividad_3.ino
@@ -0,0 +1,37 @@
+#define L1 PB4
+#define L2 PB5
+#define L3 PB6
+#define S1 PB15
+#define S2 PB14
+
+void setup() {
+  pinMode(L1, OUTPUT);
+  pinMode(L2, OUTPUT);
+  pinMode(L3, OUTPUT);
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+  afio_cfg_debug_ports(AFIO_DEBUG_SW_ONLY);
+  Serial.begin(9600);
+}
+
+void loop() {
+  delay(50);
+  uint8 wL1 = LOW;
+  uint8 wL2 = LOW;
+  uint8 wL3 = LOW;
+  uint8 rS1 = digitalRead(S1);
+  uint8 rS2 = digitalRead(S2);
+  switch (rS1 << 1 | rS2) {
+    case 0:
+      wL1 = HIGH;
+    case 1:
+      wL2 = HIGH;
+    case 2:
+      wL3 = HIGH;
+      break;
+  }
+  digitalWrite(L1, wL1);
+  digitalWrite(L2, wL2);
+  digitalWrite(L3, wL3);
+  Serial.write("SW1: " + rS1 + " SW2: " + rS2);
+}
