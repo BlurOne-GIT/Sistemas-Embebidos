131c0b15eca0c8c9fabfaaf31aebf10933431e8c
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-09-09 09:19:39 -0300

    feat(STM/TP1): actividad 5

---
+++ STM/TP1/actividad_5/actividad_5.ino
@@ -0,0 +1,14 @@
+#define L3 PB6
+#define LDR PA1
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PB5, OUTPUT);
+  pinMode(LDR, INPUT_ANALOG)
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(50);
+  analogWrite(L3, analogRead(LDR) >> 4);
+}
